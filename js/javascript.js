9455ad744e3a4f89e5c4535742bfe957c062824f
Lguilherme3 <Gui_3@outlook.com>
2022-10-10 20:36:08 -0300

    10° Funcionalidades dos botões de mostrar resultados

--- js/javascript.js
+++
@@ -1,7 +0,0 @@
-function botao() {
-    if (secondColumnGroup.style.display == 'block') {
-        secondColumnGroup.style.display = 'none'
-    } else {
-        secondColumnGroup.style.display = 'block'
-    }
-}
---
+++ js/script.js
@@ -0,0 +1,47 @@
+// botão para mostrar o resultados das oitavas de final
+
+function botaoOitavas() {
+    if (secondColumnGroup.style.display == 'block') {
+        secondColumnGroup.style.display = 'none';
+    } else {
+        secondColumnGroup.style.display = 'block';
+    }
+
+    if (seventColumnGroup.style.display == 'block') {
+            seventColumnGroup.style.display = 'none';
+    } else {
+            seventColumnGroup.style.display = 'block';
+    }
+}
+
+// botão para mostrar o resultados das quartas de final
+
+function botaoQuartas() {
+    if (thirdColumnGroup.style.display == 'block') {
+        thirdColumnGroup.style.display = 'none';
+    } else {
+        thirdColumnGroup.style.display = 'block';
+    }
+
+    if (sixthColumnGroup.style.display == 'block') {
+        sixthColumnGroup.style.display = 'none';
+    } else {
+        sixthColumnGroup.style.display = 'block';
+    }
+}
+
+// botão para mostrar o resultados das semifinal
+
+function botaoSemi() {
+    if (fourthColumnGroup.style.display == 'block') {
+        fourthColumnGroup.style.display = 'none';
+    } else {
+        fourthColumnGroup.style.display = 'block';
+    }
+
+    if (fifthColumnGroup.style.display == 'block') {
+        fifthColumnGroup.style.display = 'none';
+    } else {
+        fifthColumnGroup.style.display = 'block';
+    }
+}
