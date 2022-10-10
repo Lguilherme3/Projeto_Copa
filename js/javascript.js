1765c7a5c7ac0a259053fdc2b913207ec28404b8
Lguilherme3 <Gui_3@outlook.com>
2022-10-10 20:06:09 -0300

    9° começando algo com Js

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function botao() {
+    if (secondColumnGroup.style.display == 'block') {
+        secondColumnGroup.style.display = 'none'
+    } else {
+        secondColumnGroup.style.display = 'block'
+    }
+}
